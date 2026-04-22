d4da29f1362edd535d7dae2847f79a29054bbabf
gimenezhiago <hiagogimenezvieira888@gmail.com>
2026-04-22 11:54:20 -0300

    jhg

--- ExercicioOO/estoque.js
+++ ExercicioOO/estoque.js
@@ -1,5 +1,18 @@
 class Estoque {
-    constructor() {
-        
+  constructor() {
+    this.produtos = [];
+  }
+
+  cadastrar(nome, quantidade) {
+    if (nome === produto.nome) {
+      console.log("Produto já cadastrado.");
+      return;
+    } else {
+      const produto = {
+        nome: nome,
+        quantidade: quantidade,
+      };
+      this.produtos.push(produto);
     }
+  }
 }
