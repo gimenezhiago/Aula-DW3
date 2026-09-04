3c5279c8b235f57ba8145dcd54e68dc3787004ce
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2026-09-04 17:10:18 -0300

    sdfghj

---
+++ MVC/src/database/schema.js
@@ -0,0 +1,14 @@
+import { boolean, integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core'
+
+export const projetos = pgTable('projetos', {
+  id: serial('id').primaryKey(),
+  nome: text('nome').notNull(),
+  criadoEm: timestamp('criado_em', { withTimezone: true }).notNull().defaultNow()
+})
+
+export const tarefas = pgTable('tarefas', {
+  id: serial('id').primaryKey(),
+  descricao: text('descricao').notNull(),
+  concluido: boolean('concluido').notNull().default(false),
+  projetoId: integer('projeto_id')
+})
